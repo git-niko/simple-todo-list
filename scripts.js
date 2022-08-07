7d39a9895169f657c7c14eb9c0be91e7de2fba60
nicolepeterson <nicolepeterson.b@gmail.com>
2022-08-06 20:51:05 -0700

    Added comments to scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
+"use strict";
+
 let items;
 let btn;
 
+// *************** Add List Item *************** 
 function addListItem() {
 
     let ul = document.querySelector("ul");
@@ -8,10 +11,14 @@ function addListItem() {
     let newListItem = document.createElement("li");
     let errMsg = document.querySelector(".err-msg");
 
+    // check if input is empty
     if (userInput.value === "") {
         errMsg.classList.add("error");
         errMsg.innerHTML = "Please enter a to do list item"
     } else {
+        // if not empty, add the value to the new li
+        // put focus back to input field & remove error msg
+        // call addEventToListItems()
         newListItem.innerHTML = userInput.value;
         ul.appendChild(newListItem);
         userInput.value = "";
@@ -21,14 +28,16 @@ function addListItem() {
     }
 }
 
+// *************** Remove List Item *************** 
 function removeListItem(event) {
+    // add a line through the list item & remove after 1 sec
     event.target.classList.add("li-strikethrough");
     setTimeout(() => {
         event.target.remove();
     }, 1000)
 }
 
-
+// *************** Event Listeners *************** 
 btn = document.getElementById("add-new").addEventListener("click", addListItem);
 
 function addEventToListItems(items) {
